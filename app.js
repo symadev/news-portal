6844a60ed515778cddf5fa5be931ea0e26b61273
syma sultana <symasultana2002@gmail.com>
2025-02-13 17:37:58 +0600

    set the nav ber

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const loadCategory = async() =>{
 
     const response =  await fetch("https://openapi.programming-hero.com/api/news/categories");
     const data = await response.json();
-    console.log(data);
+    const categoryBarContainer = document.getElementById("category-bar-container");
+   data.data.news_category.forEach((item) => {
+const div = document.createElement("div");
+div.innerHTML=`<button class="btn btn-ghost">${item.category_name}</button> `;
+    
+    
+
+categoryBarContainer.appendChild(div)
+   });
 
 }
 
