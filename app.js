4c94ef82f5814d2d2e396c0dc680140419a37962
syma sultana <symasultana2002@gmail.com>
2025-02-13 20:25:49 +0600

    set the dynamic images

--- app.js
+++ app.js
@@ -10,9 +10,44 @@ div.innerHTML=`<button class="btn btn-ghost">${item.category_name}</button> `;
     
     
 
-categoryBarContainer.appendChild(div)
-   });
+categoryBarContainer.appendChild(div);
+});
 
-}
+};
 
+
+
+const loadNews = async() =>{
+
+
+    const response =  await fetch("https://openapi.programming-hero.com/api/news/category/01");
+    const data = await response.json();
+    const newsContainer = document.getElementById("news-container");
+data.data.forEach((item) => {
+    const div = document.createElement("div");
+    div.innerHTML=` <div class="hero-content flex-col lg:flex-row">
+              <img
+                src=${item.thumbnail_url}
+                class="max-w-sm  h-[300px] rounded-lg " />
+              <div>
+                <h1 class="text-3xl font-bold">${item.title}</h1>
+                <p class="py-6">
+                  ${item.details.slice(0,250)}  
+                </p>
+                <button class="btn btn-primary">Show Details</button>
+              </div>
+            </div>
+                
+          
+      `;
+
+      newsContainer.appendChild(div);
+});
+
+
+
+};
+
+
+loadNews();
 loadCategory();
