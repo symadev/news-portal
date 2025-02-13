73e1a749ce26e14023f35e067e0e2bc66fea6f84
syma sultana <symasultana2002@gmail.com>
2025-02-13 17:08:27 +0600

    edit the image

---
+++ app.js
@@ -0,0 +1,10 @@
+const loadCategory = async() =>{
+
+
+    const response =  await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await response.json();
+    console.log(data);
+
+}
+
+loadCategory();
